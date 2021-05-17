1c70438cad2bcbc0e1615d68dbcfc0f476df2490
Amjad <84246684+amjadcp@users.noreply.github.com>
2021-05-18 03:33:59 +0530

    Update webpage_generator.py

--- webpage_generator.py
+++ webpage_generator.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python
 
-from goto import goto, label
-import subprocess
+""" Author : Amjad
+    Date   : 18 May 2021 3:19 AM
+    About  : A small python program to generate a sample webpage
+"""
+
+from goto import goto, label #importing goto and label module
+import subprocess             #subprocess module to intaract with terminal
 
 
-def option_1():
+def option_1(): #fuction1
     try:
         title = input("What will be title of your webpage? => ")
         name = input("What is your name? => ")
@@ -35,10 +40,10 @@ def option_1():
          goto .start_2
     except ValueError:
         print("oops not applicable try again !")
-        goto .start_3
+        goto .start_3                                      #fuction1 end
 
 
-def option_2():
+def option_2():                                                       #fuction2 to create sample form page
     title = input("What will be title of your webpage? => ")
     print("""Hey, your sample form created please check generate.html""")
     file.write("""<html>
@@ -51,15 +56,17 @@ def option_2():
     Age &nbsp;&nbsp; : &nbsp;&nbsp; <input type="text"><br>
     Sex &nbsp;&nbsp; : &nbsp;&nbsp; <input type="radio" name="sex"> &nbsp; M &nbsp; <input type="radio" name="sex"> F<br>
     Phone Number &nbsp;&nbsp; : &nbsp;&nbsp; <input type="text"><br>
-    Address &nbsp;&nbsp; : &nbsp;&nbsp; <textarea>address</textarea> 
+    Address &nbsp;&nbsp; : &nbsp;&nbsp; <textarea>address</textarea> <br><br>
+    <input type="submit" value="Submit"> &nbsp; &nbsp; <input type="reset" value="Clear">
     </form>
     </body>
     </html>""")
-    file.close()
+    file.close()            #fuction2 end
 
 
+         
  label .start_6
-print("""## This is a small application to create a sample webpage based on ur answers
+print("""## This is a small application to create a sample webpage based on ur answers        #main start
 
      +++++++++++++++++++++++++++++++++++++++++++++++++
                         MENU
@@ -68,8 +75,9 @@ print("""## This is a small application to create a sample webpage based on ur a
                2 : To create a form to add your personal data
                """)
 
+
 label .start_1
-option = int(input("Enter your option => "))
+option = int(input("Enter your option => "))    #here receiving and checking you option
 file = open("generate.html", "w")
 if option == 1:
     option_1()
@@ -79,8 +87,10 @@ else:
     print("Wrong option try again")
      goto .start_1
 
+
+
 label .start_4
-switch = input("Do you want to change this html code manualy? (y/n)")
+switch = input("Do you want to change this html code manualy? (y/n)")     #here asking user need to open code in editor or not
 if switch == "y" or switch == "Y":
     subprocess.call("nano generate.html", shell=True)
 elif switch == "n" or switch == "N":
@@ -89,8 +99,10 @@ else:
     print("oops not applicable please try!")
     goto .start_4
 
+
+    
 label .start_5
-switch = input("Do you want to run this application again? !!! your data will be lose !!! (y/n)")
+switch = input("Do you want to run this application again? !!! your data will be lose !!! (y/n)")    #here asking to usr need this program run again
 if switch == "y" or switch == "Y":
     goto .start_6
 elif switch == "n" or switch == "N":
